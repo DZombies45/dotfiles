20e30c9bf49f0f5e99ee4da124faf6d50111bb2c
jombie <dewan.dinda@gmail.com>
2026-06-29 14:55:56 +0700

    Revert "trying change mc"
    This reverts commit a64a549c282908e5fd8fe953504f7860dda61a29.

--- home/.config/shell/alias.sh
+++ home/.config/shell/alias.sh
@@ -1,50 +1,18 @@
-# Shortcut
 alias T="cd ~/storage/downloads/Termux/"
 alias ls="lsd -aAF --group-dirs=first"
 alias vim="nvim"
 alias npr="npm run"
 alias lg="lazygit"
+alias format='prettier --write "**/*.{js,ts,json}"'
+alias pserver="python -m http.server 8000 | am start --user 0 -a android.intent.action.VIEW -d 'http://localhost:8000' | termux-notification -t 'Web Server Aktif' -c 'Buka http://localhost:8000'"
 alias cpd="rsync -av --progress --exclude node_modules "
 alias cat='bat --paging=never --theme=tokyonight_night'
-alias format='prettier --write "**/*.{js,ts,json}"'
-alias rm="rm -I --preserve-root"
-alias cp="cp -i"
-alias mv="mv -i"
-alias duit="node ~/.Duit/index.mjs"
-
-# Python and Server stuff
-alias pserver="python -m http.server 8000 | am start --user 0 -a android.intent.action.VIEW -d 'http://localhost:8000' | termux-notification -t 'Web Server Aktif' -c 'Buka http://localhost:8000'"
 alias pyserve="pkill -f server.py 2>/dev/null; python3 server.py > /dev/null 2>&1 & sleep 2 && am start -a android.intent.action.VIEW -d 'http://localhost:8000'"
 alias pyservede="pkill -f server.py 2>/dev/null; python3 server.py & sleep 2 && am start -a android.intent.action.VIEW -d 'http://localhost:8000'"
 alias pystop="pkill python"
 alias ping="ping -c 6"
-alias hserve="am start --user 0 -a android.intent.action.VIEW -d 'http://localhost:8000' && hugo server --minify --noHTTPCache --port 8000"
-
-# Package update
 alias update="pkg update && pkg upgrade"
-alias updatelist='pkg update && echo "---- upgradable pkg ----" && apt list --upgradable'
-reload() {
-    current_shell=$(basename "$SHELL")
-    
-    clear
-    
-    echo "🔄 Reloading $current_shell configuration..."
-    
-    case "$current_shell" in
-        bash)
-            # Reload bash config
-            source ~/.bashrc
-            ;;
-        zsh)
-            # Reload zsh config  
-            source ~/.zshrc
-            ;;
-        *)
-            echo "❌ Unknown shell: $current_shell"
-            return 1
-            ;;
-    esac
-    
-    echo "✅ Reload complete! Aliases and functions updated."
-}
-alias rld='reload'
+alias rm="rm -I --preserve-root"
+alias cp="cp -i"
+alias mv="mv -i"
+alias hserve="hugo server --minify --noHTTPCache --port 8000 && am start --user 0 -a android.intent.action.VIEW -d 'http://localhost:8000'"
---
+++ mc/.config/zsh/zshrc.d/completion.zsh
@@ -0,0 +1,31 @@
+# pastikan custom completions dibaca sebelum oh-my-zsh
+# fpath=(
+#   $HOME/.config/zsh/completions
+#   $fpath
+# )
+# Inisialisasi completion system
+
+# autoload -Uz compinit
+# Skip security check untuk completion cache yang sudah ada
+# compinit -u
+
+
+
+# if [ -f "$HOME/.config/zsh/completions/_mc-update" ]; then
+#     autoload -Uz _mc-update
+#     compdef _mc-update mc-update
+# fi
+# if [ -f "$HOME/.config/zsh/completions/_mc-test"]; then
+#     autoload -Uz _mc-test
+#     compdef _mc-test mc-test
+# fi
+# if [ -f "$HOME/.config/zsh/completions/_mc-export"]; then
+#     autoload -Uz _mc-export
+#     compdef _mc-export mc-export
+# fi
+# if [ -f "$HOME/.config/zsh/completions/_mc-import"]; then
+#     autoload -Uz _mc-import
+#     compdef _mc-import mc-import
+# fi
+#
+# oh-my-zsh sudah memanggil compinit
